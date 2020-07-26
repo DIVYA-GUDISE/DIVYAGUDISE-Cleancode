7b16aa44f146ac7753cf168fb2e78aed5a5047a7
DIVYA-GUDISE <61285249+DIVYA-GUDISE@users.noreply.github.com>
2020-07-26 10:39:29 +0530

    Create CompoundInterest.java

---
+++ CompoundInterest.java
@@ -0,0 +1,9 @@
+package Interest;
+public class CompoundInterest{
+	public float findCInterest(float p,int time,int r,int times)
+{	
+float amount=p*math.pow(1+r/times),times*time);
+float compoundinterest=amount-principal;
+return compoundinterest;
+}
+}
